af5e04b48c473da13a01f7c6cd98d14dae7a55c3
gloriiakang <lori.vihchoi@gmail.com>
2021-07-21 20:20:07 -0300

    Listando as empresas cadastradas

--- src/main/java/br/com/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/gerenciador/servlet/Banco.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa0 = new Empresa();
+		empresa0.setNome("Google");
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Apple");
+		lista.add(empresa0);
+		lista.add(empresa1);
+	}
 
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
